7b4a9cbe83d5ac4971d0d659229d3c639b7e9660
Tsung Hung <hungmasterj@gmail.com>
2014-10-16 12:10:13 -0700

    simple testing for home page worked and now expanding and making templates

--- test_driven/lists/tests.py
+++ test_driven/lists/tests.py
@@ -1,7 +1,22 @@
+from django.core.urlresolvers import resolve
+from django.test import TestCase
+from lists.views import home_page
+from django.http import HttpRequest
+
 from django.test import TestCase
 
 # Create your tests here.
 
-class EagerTest(TestCase):
-	def test_bad_math(self):
-		self.assertEqual(1 + 1, 3)
+class HomePageTest(TestCase):
+
+	def test_root_url_resolves_to_home_page_views(self):
+		found = resolve('/')
+		self.assertEqual(found.func, home_page)
+
+
+	def test_home_page_returns_correct_html(self):
+		request = HttpRequest()
+		response = home_page(request)
+		self.assertTrue(response.content.startswith(b'<html>')) # b' is for raw bytes
+		self.assertIn(b'<title>To-Do lists</title>', response.content)
+		self.assertTrue(response.content.endswith(b'</html>'))
