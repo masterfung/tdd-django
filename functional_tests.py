cadea0d0add2b7bbd08b366ba2b294456d95dd36
Tsung Hung <hungmasterj@gmail.com>
2014-10-16 18:46:10 -0700

    comments on functional and updated html to allow more functional test to pass

--- functional_tests.py
+++ functional_tests.py
@@ -1,19 +1,53 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import unittest
 
 
 class NewVisitorTest(unittest.TestCase):
-    def setup(self):
-        self.browser = webdriver.Firefox()
 
-    def tear_down(self):
-        browser.quit()
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+        self.browser.implicitly_wait(3)
+
+    def tearDown(self):
+        self.browser.quit()
 
     def test_can_start_a_list_and_retrieve_it_later(self):
+        # Henry has heard about a cool new online to-do app. he goes
+        # to check out its homepage
         self.browser.get('http://localhost:8000')
 
-        self.assertIn('Awesome World!', self.browser.title)
-        self.fail('Finished the test!')
+        # he notices the page title and header mention to-do lists
+        self.assertIn('To-Do', self.browser.title)
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
+
+        # he is invited to enter a to-do item straight away
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+                inputbox.get_attribute('placeholder'),
+                'Enter a To-Do Item Here'
+        )
+
+        # he types "Buy peacock feathers" into a text box (Henry's hobby
+        # is tying fly-fishing lures)
+        inputbox.send_keys('Buy peacock feathers')
+
+        # When he hits enter, the page updates, and now the page lists
+        # "1: Buy peacock feathers" as an item in a to-do list table
+        inputbox.send_keys(Keys.ENTER)
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '1: Buy peacock feathers' for row in rows),
+            "New to-do item did not appear in table!"
+        )
+
+        # There is still a text box inviting her to add another item. he
+        # enters "Use peacock feathers to make a fly" (Henry is very
+        # methodical)
+        self.fail('Finish the test!')
 
 if __name__ == '__main__':
     unittest.main(warnings='ignore')
