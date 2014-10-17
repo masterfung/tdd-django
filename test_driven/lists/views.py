cd237f6f9b32eab82dc0828dd812d1c52a6d4cfc
Tsung Hung <hungmasterj@gmail.com>
2014-10-17 10:30:35 -0700

    updated and refactored several code bits, splitted each test to focus on one matter onlyg

--- test_driven/lists/views.py
+++ test_driven/lists/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 from .models import Item
@@ -7,11 +7,8 @@ from .models import Item
 
 def home_page(request):
     if request.method == 'POST':
-        new_item_text = request.POST['item_text']
-        Item.objects.create(text=new_item_text)
-    else:
-        new_item_text = ''
+        Item.objects.create(text=request.POST['item_text'])
+        return redirect('/')
 
-    return render(request, 'home.html', {
-        'new_item_text': new_item_text
-    })
+    items = Item.objects.all()
+    return render(request, 'home.html', {'items': items})
