63a53b4f20e82c68fbbaf994a23183a9752cbcb9
Tsung Hung <hungmasterj@gmail.com>
2014-10-17 18:02:33 -0700

    updated and refactored the test...now working to update FT

--- test_driven/lists/views.py
+++ test_driven/lists/views.py
@@ -6,13 +6,14 @@ from .models import Item
 
 
 def home_page(request):
-    if request.method == 'POST':
-        Item.objects.create(text=request.POST['item_text'])
-        return redirect('/lists/the-best/')
-
     return render(request, 'home.html')
 
 
 def the_best(request):
     items = Item.objects.all()
     return render(request, 'list.html', {'items': items})
+
+
+def new_list(request):
+    Item.objects.create(text=request.POST['item_text'])
+    return redirect('/lists/the-best/')
--- test_driven/test_driven/urls.py
+++ test_driven/test_driven/urls.py
@@ -9,4 +9,5 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^$', 'lists.views.home_page', name='home'),
     url(r'^lists/the-best/$', 'lists.views.the_best', name='the_best'),
+    url(r'^lists/new$', 'lists.views.new_list', name='new_list'),
 )
