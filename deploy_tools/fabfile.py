278ee46e990e4dd19ed73ed3e2adc33d7f4016fa
Tsung Hung <hungmasterj@gmail.com>
2014-10-21 01:41:58 -0700

    updated fabfile but more coming soon

--- deploy_tools/fabfile.py
+++ deploy_tools/fabfile.py
@@ -5,6 +5,7 @@ import random
 
 REPO_URL = 'https://github.com/masterfung/tdd-django.git'
 
+
 def deploy():
     site_folder = '/home/%s/sites' % (env.user, env.host)
     source_folder = site_folder + '/source'
@@ -14,3 +15,8 @@ def deploy():
     _update_virtualenv(source_folder)
     _update_static_files(source_folder)
     _update_database(source_folder)
+
+
+def _create_directory_structure_if_necessary(site_folder):
+    for subfolder in ('database', 'static', 'virtualenv', 'source'):
+        run('mkdir -p %s/%s' % (site_folder, subfolder))
