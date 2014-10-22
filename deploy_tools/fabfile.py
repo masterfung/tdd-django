8f88d1c43d40a7ac91cbf666968c98fd4433d008
Tsung Hung <hungmasterj@gmail.com>
2014-10-21 17:11:35 -0700

    updated fabfile

--- deploy_tools/fabfile.py
+++ deploy_tools/fabfile.py
@@ -20,3 +20,27 @@ def deploy():
 def _create_directory_structure_if_necessary(site_folder):
     for subfolder in ('database', 'static', 'virtualenv', 'source'):
         run('mkdir -p %s/%s' % (site_folder, subfolder))
+
+
+def _get_latest_source(source_folder):
+    if exists(source_folder + '/.git'):
+        run('cd %s && git fetch' % (source_folder,))
+    else:
+        run('git clone %s %s' % (REPO_URL, source_folder))
+    current_commit = local("git log -n 1 --format=%H", capture=True)
+    run('cd %s && git reset --hard %s' % (source_folder, current_commit))
+
+def _update_settings(source_folder, site_name):
+    settings_path = source_folder + "/test_driven/settings.py"
+    sed(settings_path, "DEBUG = TRUE", "DEBUG = False")
+    sed(settings_path,
+        "ALLOWED_HOSTS = .+$",
+        "ALLOWED_HOSTS = ['%s']" % (site_name,))
+
+
+    secret_key_file = source_folder + '/test_driven/secret_key.py'
+    if not exists(secret_key_file):
+        chars = 'abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(-_=+)'
+        key = ''.join(random.SystemRandom().choice(chars) for _ in range(50))
+        append(secret_key_file, "SECRET_KEY = '%s'" % (key,))
+    append(settings_path, '\nfrom .secret_key import SECRET_KEY')
